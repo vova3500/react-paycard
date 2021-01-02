5130de3c07bb7da64a992ce70897e53f11ba85e4
vova3500 <41364337+vova3500@users.noreply.github.com>
2021-01-02 21:28:29 +0300

    componet Card new style

--- src/MainScreen/components/card/index.jsx
+++ src/MainScreen/components/card/index.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
+import "./card.scss";
+
+import chip from "../../../img/chip.png";
+import visa_logo from "../../../img/visa_logo.png";
+
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="card">
+      <div className="card-header">
+        <div className="card-chip">
+          <img src={chip} alt="chip" />
+        </div>
+        <div className="card-logo">
+          <img src={visa_logo} alt="logo" />
+        </div>
+      </div>
+
+      <div className="card-number">
+        <div>#### **** **** ####</div>
+      </div>
+
+      <div className="card-bottom">
+        <div className="card-bottom-item">
+          <div>Card Holder</div>
+          <div>FULL NAME</div>
+        </div>
+        <div className="card-bottom-item">
+          <div>Expires</div>
+          <div>MM/YY</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
